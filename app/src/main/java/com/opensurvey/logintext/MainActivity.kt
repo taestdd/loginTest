876050173cf2461e0132ad420d17c7cf683fe750
hong <rlaxoxo5252@gmail.com>
2023-01-14 22:44:09 +0900

    스트링 가공 및 기능 붙이기

--- app/src/main/java/com/opensurvey/logintext/MainActivity.kt
+++ app/src/main/java/com/opensurvey/logintext/MainActivity.kt
@@ -2,10 +2,37 @@ package com.opensurvey.logintext
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainBtn.setOnClickListener{
+
+            val inputEmail = emailEdt.text.toString()
+            val inputPass = passEdt.text.toString()
+
+            if (inputEmail == "admin@test.com" && inputPass == "qwer") {
+
+                val name = "태홍"
+
+                Toast.makeText(this, "${name.toString()} 관리자입니다.", Toast.LENGTH_SHORT).show()
+            }
+
+            else  {
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+            }
+
+
+
+        }
+
+
+
+
+
     }
 }
