6e0c1c59de602caa3cfd062494b4a7876cde8ba4
hong <rlaxoxo5252@gmail.com>
2023-01-18 21:55:53 +0900

    길게누르는것도 함

--- app/src/main/java/com/opensurvey/logintext/MyList.kt
+++ app/src/main/java/com/opensurvey/logintext/MyList.kt
@@ -2,12 +2,15 @@ package com.opensurvey.logintext
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.Toast
 import com.opensurvey.logintext.adeptors.BlackAdapter
 import com.opensurvey.logintext.hongsdatas.Black
 import kotlinx.android.synthetic.main.activity_my_list.*
+import kotlinx.android.synthetic.main.activity_my_list.view.*
 
 class MyList : AppCompatActivity() {
 
@@ -32,12 +35,25 @@ class MyList : AppCompatActivity() {
 
         blackList.adapter = mAdapter
 
+
         blackList.setOnItemClickListener { parent, view, position, id ->
 
             val clickedBlack = mBlackList[position]
             Toast.makeText(this, "${clickedBlack.name}입니다.", Toast.LENGTH_SHORT).show()
+        }
+
+        blackList.setOnItemLongClickListener { parent, view, position, id ->
+
+            val longClickedBlack = mBlackList[position]
+            Toast.makeText(this, "${longClickedBlack.name}입니다람쥐.", Toast.LENGTH_SHORT).show()
+
+            return@setOnItemLongClickListener true
 
         }
 
+
+
     }
 }
+
+
