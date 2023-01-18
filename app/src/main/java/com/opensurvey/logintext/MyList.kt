a30f7b8cb1f621fc7ab32da1737f36df37189d78
hong <rlaxoxo5252@gmail.com>
2023-01-18 20:57:10 +0900

    학생 목록을 ArrayList 생성, 실제 목록 추가

--- app/src/main/java/com/opensurvey/logintext/MyList.kt
+++ app/src/main/java/com/opensurvey/logintext/MyList.kt
@@ -2,10 +2,23 @@ package com.opensurvey.logintext
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.opensurvey.logintext.hongsdatas.Black
 
 class MyList : AppCompatActivity() {
+
+    val mBlackList = ArrayList<Black>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_list)
+
+        mBlackList.add( Black("김태홍","1990") )
+        mBlackList.add( Black("김태홍1","1991") )
+        mBlackList.add( Black("김태홍2","1992") )
+        mBlackList.add( Black("김태홍3","1993") )
+        mBlackList.add( Black("김태홍4","1994") )
+        mBlackList.add( Black("김태홍5","1995") )
+        mBlackList.add( Black("김태홍6","1996") )
+
     }
 }
--- app/src/main/java/com/opensurvey/logintext/adeptors/BlackAdapter.kt
+++ app/src/main/java/com/opensurvey/logintext/adeptors/BlackAdapter.kt
@@ -1,10 +1,32 @@
 package com.opensurvey.logintext.adeptors
 
 import android.content.Context
+import android.provider.DocumentsContract.Root
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.opensurvey.logintext.R
 import com.opensurvey.logintext.hongsdatas.Black
 
-class BlackAdapter(val mContext : Context,
-                   val resId : Int,
-                   val mList : ArrayList<Black>) : ArrayAdapter<Black>(mContext, resId, mList) {
+class BlackAdapter(
+    val mContext : Context,
+    val resId : Int,
+    val mList : ArrayList<Black>) : ArrayAdapter<Black>(mContext, resId, mList) {
+
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var temprow = convertView
+        if (temprow == null) {
+
+            temprow = inf.inflate(R.layout.black_list_item, null) }
+        val row = temprow!!
+
+        return row
+
+    }
+
 }
